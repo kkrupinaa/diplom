035173111c6e623e9b3c084928a41c17d21e58b4
kkrupinaa <79110379+kkrupinaa@users.noreply.github.com>
2022-05-10 22:06:22 +0300

    work with api

---
+++ new-app/public/musicbox.ts
@@ -0,0 +1,38 @@
+/**
+ * Альбом
+ */
+export class MusicBox{
+    photo:string
+    private id:string
+    first_title:string
+    second_title:string
+    tracks:JSON
+    constructor(photo:string,first_title:string, second_title:string,tracks:JSON){
+        this.id='${Date.now()}_${Math.floor(Math.random() * 1000)}'
+        this.photo=photo
+        this.first_title=first_title
+        this.second_title=second_title
+        this.tracks=tracks
+    }
+    /**
+     * HTML представление
+     * @param musicbox экземпляр MusicBox
+     * @returns HTML представление экземпляра класса MusicBox
+     */
+    static template(musicbox:MusicBox):string{
+        return `
+        <div class="music-box" data-id=${musicbox.id}>
+            <div class="album">
+                <div class="photo">
+                  <div class="album_photo">
+                    <img src=${musicbox.photo} height="148px" style="box-shadow: 0px 0px 25px 2px #000; align-self:center">
+                  </div>
+                <img class="hidden-img"src="./play-button.svg" width="48px"height=48px>
+                </div>
+                <div class="first-title">${musicbox.first_title}</div>
+                <div class="second-title">${musicbox.second_title}</div>
+            </div>
+        </div>
+        `
+    }
+}
