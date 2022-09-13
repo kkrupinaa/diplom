573729e6d6398e065aa1067fc3fbf58e95d4de26
kkrupinaa <79110379+kkrupinaa@users.noreply.github.com>
2022-09-13 22:09:49 +0300

    fixes in hook

--- my-app/src/components/hooks/useDataFetch.ts
+++ my-app/src/components/hooks/useDataFetch.ts
@@ -4,19 +4,23 @@ import { useEffect, useState } from "react"
 export type UseDataFetch<T> = {
     data: T | undefined
     responseStatus: number | undefined
+    errorMessage: string
+    loadStatus: Status
 }
+export type Status = "loading" | "loaded" | "error"
 /**
  * Запрос серверу
  * @param url ссылка
  */
-export const fetchData = async (url: string) => {
+export const fetchData = async (url: string, signal: AbortSignal) => {
     const accessToken = localStorage.getItem('access_token')
     const res = await fetch(url, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + accessToken
-        }
+        },
+        signal: signal
     })
     const status = res.status
     const data = await res.json()
@@ -28,19 +32,41 @@ export const fetchData = async (url: string) => {
 export const useDataFetch = <T>(url: string, token: string | null): UseDataFetch<T> => {
     const [result, setResult] = useState<UseDataFetch<T>>({
         data: undefined,
-        responseStatus: undefined
+        responseStatus: undefined,
+        errorMessage: '',
+        loadStatus: 'loading'
     })
     useEffect(() => {
-        fetchData(url)
+        setResult((prev) => {
+            return {
+                ...prev,
+                loadStatus: 'loading'
+            }
+        })
+        const controller = new AbortController()
+        fetchData(url, controller.signal)
             .then((data) => {
-                setResult(data)
+                setResult(
+                    {
+                        errorMessage: '',
+                        data: data.data,
+                        responseStatus: data.responseStatus,
+                        loadStatus: 'loaded'
+                    }
+                )
             })
-            .catch(({ status }) => {
+            .catch((reason) => {
                 setResult({
                     data: undefined,
-                    responseStatus: status
-                })
+                    errorMessage: reason,
+                    loadStatus: 'error',
+                    responseStatus: undefined
+                }
+                )
             })
+        return () => {
+            controller.abort()
+        }
     }, [url, token])
     return result
 }
